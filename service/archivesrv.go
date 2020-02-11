fcdf6ef7d5e0dfbfd891e71bc696b5e225ef6a5e
concurrent <concurrent.exec@gmail.com>
2020-02-12 00:40:48 +0800

    通过链接去重

--- service/archivesrv.go
+++ service/archivesrv.go
@@ -3,19 +3,20 @@ package service
 import "github.com/arrebole/databox/model"
 
 // SaveArchive ...
-func SaveArchive(archive *model.Archive) bool{
-	if !exist(archive.Title){
+func SaveArchive(archive *model.Archive) bool {
+	if !exist(archive.Link) {
 		db.Create(archive)
 		return true
 	}
 	return false
 }
 
-func exist(title string) bool {
+// exist 判断链接是否已存在
+func exist(link string) bool {
 	var archives []model.Archive
 	var count = 0
-	db.Where("title = ?", title).Find(&archives).Count(&count)
-	if count > 0{
+	db.Where("link = ?", link).Find(&archives).Count(&count)
+	if count > 0 {
 		return true
 	}
 	return false
